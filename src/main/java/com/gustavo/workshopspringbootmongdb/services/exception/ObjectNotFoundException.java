1f42a5fbf721d3f75cb835f0bc93269d1a4c1eba
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2022-02-05 20:33:00 -0300

    Getting a user by id

---
+++ src/main/java/com/gustavo/workshopspringbootmongdb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.gustavo.workshopspringbootmongdb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
