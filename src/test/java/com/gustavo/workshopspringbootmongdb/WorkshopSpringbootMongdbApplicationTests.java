b7c9dd10ef4ea816d3d9b570e8d4765cd2040a3b
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2022-02-05 12:34:03 -0300

    Project created

---
+++ src/test/java/com/gustavo/workshopspringbootmongdb/WorkshopSpringbootMongdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.workshopspringbootmongdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
