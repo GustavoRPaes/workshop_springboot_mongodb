50074c617cbe5f16b821d6208965420cf297b0b8
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2022-02-06 20:12:05 -0300

    Query with several criteria and some fixes

--- src/main/java/com/gustavo/workshopspringbootmongdb/domain/Post.java
+++ src/main/java/com/gustavo/workshopspringbootmongdb/domain/Post.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -14,18 +13,17 @@ import com.gustavo.workshopspringbootmongdb.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
-
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
+
 	private List<CommentDTO> comments = new ArrayList<>();
-	
+
 	public Post() {
 	}
 
@@ -69,7 +67,7 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
+
 	public AuthorDTO getAuthor() {
 		return author;
 	}
@@ -77,7 +75,7 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
-	
+
 	public List<CommentDTO> getComments() {
 		return comments;
 	}
@@ -88,7 +86,10 @@ public class Post implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -100,6 +101,11 @@ public class Post implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Post other = (Post) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
